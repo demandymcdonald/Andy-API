d02eb0fc2944f54fbbbcd2702e46d51292a222fb
demandymcdonald <demandymcdonald@gmail.com>
2024-02-07 14:15:21 -0500

    Update aapi_loader.lua

--- aapi_loader.lua
+++ aapi_loader.lua
@@ -1,22 +1,13 @@
 
 f = {}
-<<<<<<< Updated upstream
 function f.setVersion(version)
-=======
-function f.selver(version)
->>>>>>> Stashed changes
     if version == "m" then
         Version = "m"
     elseif version == "d" then
         Version = "d"
     end
-<<<<<<< Updated upstream
-end 
-function git()
-=======
 end
 function f.git()
->>>>>>> Stashed changes
     if fs.exists("/git.lua") then
         git = require("git")
         sleep(1)
@@ -30,15 +21,7 @@ function f.git()
     end
 end
 function f.core()
-<<<<<<< Updated upstream
     
-=======
-    if Version == "m" then
-        
-    elseif Version == "d" then
-
-    end
->>>>>>> Stashed changes
 end
 
 Apistartup()
