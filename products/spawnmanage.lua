1d87c1d0c240601c56d335bf6f3135c9dd4f67ba
demandymcdonald <demandymcdonald@gmail.com>
2024-02-07 16:51:02 -0500

    Update spawnmanage.lua

--- products/spawnmanage.lua
+++ products/spawnmanage.lua
@@ -4,6 +4,33 @@ Aapi.initDebug(DebugLogFiles)
 Aapi.dbg("hello world")
 Debugmode = true
 Aapi.PeripheralSetup()
+PD = AAPI.Pertype("playerDetector")
+Playersinspawn = {}
 function PlayerFinder()
-                
+    local players = PD.getPlayersInRange(100)¶           
+    if players then
+        for i = 1, #players do
+            --if players[i] ~= "Wolf_Obsidio" then
+            commands.exec("gamemode adventure " .. players[i])
+            table.insert(Playersinspawnm, players[i])
+            --end
+        end
+    end
+    for i = 1, #Playersinspawn do
+        local here = false
+        for e = 1, #players do
+            if Playersinspawn[i] == players[e] then
+                here = true
+                break
+            end
+        end
+        if here == false then
+            commands.exec("gamemode adventure " .. Playersinspawn[i])
+            table.remove(Playersinspawn, i)
+        end
+    end
+    sleep(5)
+end
+while true do
+    PlayerFinder()
 end
