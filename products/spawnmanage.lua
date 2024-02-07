0e1b1924eefb277a9d2e80cf80b785788c025bfc
demandymcdonald <demandymcdonald@gmail.com>
2024-02-07 16:37:01 -0500

    Update spawnmanage.lua

--- products/spawnmanage.lua
+++ products/spawnmanage.lua
@@ -1,8 +1,9 @@
 Aapi = require("aapi_core")
-print("Hello World")
 local DebugLogFiles = "SpawnManager/debuglogs/"
 Aapi.initDebug(DebugLogFiles)
 Aapi.dbg("hello world")
 Debugmode = true
 Aapi.PeripheralSetup()
-print("test2")
+function PlayerFinder()
+                
+end
