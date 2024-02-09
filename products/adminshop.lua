8b5ae805b232b6a10e760c1265a2af7b1b8646d3
demandymcdonald <demandymcdonald@gmail.com>
2024-02-09 16:06:44 -0500

    Update adminshop.lua

--- products/adminshop.lua
+++ products/adminshop.lua
@@ -34,7 +34,7 @@ function Startup()
     if PriceList == nil then
         PriceList = {}
     end
-    disp.addwindow(Mon,"Main","The Company Store",1,1,1,1,"colors:black",true)
+    disp.addWindow(Mon,"Main","The Company Store",1,1,1,1,"colors:black",true)
 end
 Startup()
 function Savelist()
